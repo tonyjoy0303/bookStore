35d372a00290717e8e0499b33932543bc339b090
tonyjoy0303 <tonyjoyjp@gmail.com>
2024-12-29 17:00:39 +0530

    Login page

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+include 'db_config.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $_SESSION['username'] = $username;
+        header("Location: dashboard.php");
+    } else {
+        echo "Invalid username or password";
+    }
+}
+?>
